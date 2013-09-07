726c4cc826cdfe33faa79ddf6ea23fb462898c8a
Juan Ejemplo <juan@ejemplo.com>
2013-09-07 14:22:37 -0300

    Envío estructura limpia de la aplicación Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prototipo::Application.config.secret_key_base = 'da16678d17931f1c9a089c0328308efea014eb4a6dbcd6fc1d4b4514e29ff835fe033f48427f46c176464e8f271dfd8cdcd6f9f4c4a3ca157ae2d1a721f4c9af'
