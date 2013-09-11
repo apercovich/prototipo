1f5612e51714772fe316b14c269d9252c164ae9b
Leandro <lburgosabal@gmail.com>
2013-09-11 22:12:25 +0200

    Cambié una migration y corrí migrate, se actualizó el schema.rb

--- db/migrate/20130910030950_remove_active_enabled_role_from_users.rb
+++ db/migrate/20130910030950_remove_active_enabled_role_from_users.rb
@@ -1,7 +1,6 @@
 class RemoveActiveEnabledRoleFromUsers < ActiveRecord::Migration
   def change
     remove_column :users, :role, :string
-    remove_column :users, :active, :boolean
-    remove_column :users, :enabled, :boolean
+    
   end
 end
