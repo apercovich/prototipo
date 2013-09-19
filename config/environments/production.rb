876db0d4f69196575f0f260819ce0eb6098bd8b6
Ricardo <rr.ricci@gmail.com>
2013-09-19 19:28:38 -0300

    Cambios en el layout para cargar Bootstrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,9 +19,6 @@ Prototipo::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
@@ -77,4 +74,10 @@ Prototipo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  
+  # -->  Con esto logramos cargar las cosas de la carpeta assets en Heroku  <--
+  
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_assets = true
 end
