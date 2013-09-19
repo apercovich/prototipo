ffd22d80354c27a17e653a2dc550c47127ea7717
Ricardo <rr.ricci@gmail.com>
2013-09-19 19:40:04 -0300

    Cambios para cargar Bootstrap en Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Prototipo::Application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  config.assets.digest = false#true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
